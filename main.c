2e1c8a6795d5d8a2deaaaafd263a051210f4db82
hellowujiayi <w.45@qq.com>
2019-04-13 01:36:20 +0800

    Add to main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+/* GameName : Tetris
+ * Author   : WuJiaYi
+ * E-mail   : hello@wujiayi.vip
+ * Blog     : http://www.wujiayi.vip
+ * Version  : V0.1
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("hello world\n");
+    return 0;
+}
